9d55883d6e6af1823ed91fe0f0cecca2cae72d52
ShamsulArifin <arifinbd6@gmail.com>
2021-02-14 16:26:33 +0600

    added fields and logic for report generation

--- custom_addons/inventory_consolidated_report/wizard/icr_report_stock_wizard.py
+++ custom_addons/inventory_consolidated_report/wizard/icr_report_stock_wizard.py
@@ -11,4 +11,42 @@ class IcrReportStock(models.TransientModel):
     _name = "icr.report.stock"
     _description = "Inventory Consolidated Report .xlsx"
 
-    test = fields.Char('test', readonly=True)
+    @api.model
+    def get_default_date_model(self):
+        return pytz.UTC.localize(datetime.now()).astimezone(timezone(self.env.user.tz or 'UTC'))
+
+    datas = fields.Binary('File', readonly=True)
+    datas_fname = fields.Char('Filename', readonnly=True)
+    product_ids = fields.Many2many('product.product', 'icr_report_stock_product_rel', 'icr_report_stock_id',
+                                   'product_id', 'Product')
+    categ_ids = fields.Many2many('product.category', 'icr_report_stock_categ_rel', 'icr_report_stock_id',
+                                 'categ_id', 'Categories')
+    location_ids = fields.Many2many('stock.location', 'icr_report_stock_location_rel', 'icr_report_stock_id',
+                                    'location_id', 'Locations')
+
+    def print_icr_report(self):
+        data = self.read[0]
+        product_ids = data['product_ids']
+        categ_ids = data['categ_ids']
+        location_ids = data['location_ids']
+
+        if categ_ids:
+            product_ids = self.env['product.product'].search([('categ_id', 'in', categ_ids)])
+            product_ids = [prod.id for prod in product_ids]
+        where_product_ids = " 1=1 "
+        where_product_ids2 = " 1=1 "
+        if product_ids:
+            where_product_ids = " quant.product_id in %s"%str(tuple(product_ids)).replace(',)', ')')
+            where_product_ids2 = " product_id in %s"%str(tuple(product_ids)).replace(',)', ')')
+        location_ids2 = self.env['stock.location'].search([('usage', '=', 'internal')])
+        ids_location = [loc.id for loc in location_ids2]
+        where_location_ids = " quant.location_id in %s"%str(tuple(ids_location)).replace(',)', ')')
+        where_location_ids2 = " location_id in %s"%str(tuple(ids_location)).replace(',)', ')')
+        if location_ids:
+            where_location_ids = " quant.location_id in %s"%str(tuple(location_ids)).replace(',)', ')')
+            where_location_ids2 = " location_id in %s"%str(tuple(location_ids)).replace(',)', ')')
+
+        datetime_string = self.get_default_date_model().strftime("%Y-%m-%d %H:%M:%S")
+        date_string = self.get_default_date_model().strftime("%Y-%m-%d")
+        report_name = 'Inventory Consolidated Report'
+        filename = '%s %s.xlsx'%(report_name,date_string)
