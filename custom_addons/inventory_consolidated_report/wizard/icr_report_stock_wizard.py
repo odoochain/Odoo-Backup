63f1d3067740d92dd9ea438eee0f8ae8e6c46c98
ShamsulArifin <arifinbd6@gmail.com>
2021-02-15 11:10:07 +0600

    added query for data exporting to excel

--- custom_addons/inventory_consolidated_report/wizard/icr_report_stock_wizard.py
+++ custom_addons/inventory_consolidated_report/wizard/icr_report_stock_wizard.py
@@ -50,3 +50,61 @@ class IcrReportStock(models.TransientModel):
         date_string = self.get_default_date_model().strftime("%Y-%m-%d")
         report_name = 'Inventory Consolidated Report'
         filename = '%s %s.xlsx'%(report_name,date_string)
+
+        columns = [
+            ('No', 5, 'no', 'no'),
+            ('Product', 30, 'char', 'char'),
+            ('Product Category', 20, 'char', 'char'),
+            ('Location', 30, 'char', 'char'),
+            ('Incoming Date', 20, 'char', 'char'),
+            ('Stock Age', 20, 'number', 'number'),
+            ('Total Stock', 20, 'float', 'float'),
+            ('Available', 20, 'float', 'float'),
+            ('Reserved', 20, 'float', 'float'),
+        ]
+
+        datetime_format = '%Y-%m-%d %H:%M:%S'
+        utc = datetime.now().strftime(datetime_format)
+        utc = datetime.strptime(utc, datetime_format)
+        tz = self.get_default_date_model().strftime(datetime_format)
+        tz = datetime.strptime(tz, datetime_format)
+        duration = tz -utc
+        hours = duration.seconds / 60 / 60
+        if hours > 1 or hours < 1 :
+            hours = str(hours) + ' hours'
+        else:
+            hours = str(hours) + ' hour'
+
+        query = """
+            SELECT 
+                prod_tmpl.name as product, 
+                categ.name as prod_categ, 
+                loc.complete_name as location,
+                quant.in_date + interval '%s' as date_in, 
+                date_part('days', now() - (quant.in_date + interval '%s')) as aging,
+                sum(quant.qty) as total_product, 
+                sum(quant2.qty) as stock, 
+                sum(quant3.qty) as reserved
+            FROM 
+                stock_quant quant
+            LEFT JOIN 
+                (select * from stock_quant where %s and %s
+                and reservation_id is null) quant2 on quant2.id = quant.id
+            LEFT JOIN 
+                (select * from stock_quant where %s and %s
+                and reservation_id is not null)quant3 on quant3.id = quant.id
+            LEFT JOIN 
+                stock_location loc on loc.id=quant.location_id
+            LEFT JOIN 
+                product_product prod on prod.id=quant.product_id
+            LEFT JOIN 
+                product_template prod_tmpl on prod_tmpl.id=prod.product_tmpl_id
+            LEFT JOIN 
+                product_category categ on categ.id=prod_tmpl.categ_id
+            WHERE 
+                %s and %s
+            GROUP BY 
+                product, prod_categ, location, date_in
+            ORDER BY 
+                date_in
+        """
